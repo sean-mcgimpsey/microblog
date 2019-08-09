f541468d44979b0959e5fdb3fe9ff576c41781df
sean-mcgimpsey <sean.mcgimpsey@gmail.com>
2019-08-09 16:25:47 +0100

    Time functionality added

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -8,6 +8,7 @@ from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,6 +16,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
